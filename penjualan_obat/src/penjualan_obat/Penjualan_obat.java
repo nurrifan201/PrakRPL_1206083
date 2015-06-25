a355fd5674f1ef8d183edfdc6ba777855e3b8df3
nurrifan201 <1206083@sttgarut.ac.id>
2015-06-25 12:15:57 +0700

    folder java penjualan obat

---
+++ penjualan_obat/src/penjualan_obat/Penjualan_obat.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penjualan_obat;
+
+/**
+ *
+ * @author ifan
+ */
+public class Penjualan_obat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
